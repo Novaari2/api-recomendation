fae9aa31a6f9dd7897de2860af6f8fde8d2d33f7
novaari2 <novaari20@gmail.com>
2024-07-30 18:41:28 +0700

    update env

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ import pymysql.cursors
 import random
 import os
 
-db = pymysql.connect(host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
+db = pymysql.connect(
+    host=os.getenv('DB_HOST'),
+    port=int(os.getenv('DB_PORT')),
     user=os.getenv('DB_USER'),
     password=os.getenv('DB_PASSWORD'),
     db=os.getenv('DB_NAME'),
